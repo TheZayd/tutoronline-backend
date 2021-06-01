8067348cd1e6bf4efff344af331bf4853c9f22b5
TheZayd <83833070+TheZayd@users.noreply.github.com>
2021-06-02 00:58:08 +0100

    Add files via upload

---
+++ tutoronline-backend/tutoronline-api/src/main/java/com/tutoronline/api/TutoronlineApiApplication.java
@@ -0,0 +1,13 @@
+package com.tutoronline.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoronlineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoronlineApiApplication.class, args);
+	}
+
+}
---
+++ tutoronline-backend/tutoronline-api/src/test/java/com/tutoronline/api/TutoronlineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutoronline.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoronlineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
